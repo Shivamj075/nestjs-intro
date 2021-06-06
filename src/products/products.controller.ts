bab5d9548d53797e2a0ca8065b1d487d24db6e96
Shivam <shivamj075@gmail.com>
2021-06-06 11:41:19 +0530

    Add inserproduct post api to insert product

---
+++ src/products/products.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Post} from "@nestjs/common";
+import { ProductsService } from "./products.service";
+
+@Controller('products')
+export class ProductsController {
+    constructor(private readonly productsService: ProductsService ){}
+
+    @Post()
+    addProduct(
+        @Body('title') prodTitle: string,
+        @Body('description') prodDesc: string,
+        @Body('price') prodPrice: number,
+    ): any{
+         const generateId = this.productsService.insertProduct(prodTitle, prodDesc, prodPrice);
+         return {id: generateId};
+    }
+}
